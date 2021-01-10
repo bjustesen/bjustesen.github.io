3848cac1e54dc65001f454d55014611d6c006296
bjustesen <bog15004@byui.edu>
2021-01-09 17:01:54 -0700

    css

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,2 @@
-const options = {weekday: 'long', day: 'numeric', month:'long', year:'numeric'};
+const options = {day: 'numeric', month:'numeric', year:'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
