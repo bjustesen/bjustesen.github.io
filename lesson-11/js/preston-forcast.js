34be170afa166f63597850c2f272872494859c78
bjustesen <bog15004@byui.edu>
2021-03-20 14:11:25 -0600

    forcast

--- lesson-11/js/preston-forcast.js
+++ lesson-11/js/preston-forcast.js
@@ -1,25 +1,40 @@
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id={5604473}&appid={979e25236567012c3b8dd149b86738a0}';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=979e25236567012c3b8dd149b86738a0';
+const apiForcast = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=979e25236567012c3b8dd149b86738a0';
 
-
-
-fetch(apiURL)
+fetch(apiForcast)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
 
+    const list = jsObject['list'];
+
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].dt_txt.includes('18:00:00')) {
+
+        let table = document.createElement("section");
+
+
+        const imagesrc = "https://openweathermap.org/img/w/" + jsObject.list[i].weather[0].icon + ".png"; //note the concatenation
+        const desc = jsObject.list[i].weather[0].description;
+        let image = document.createElement("img");
+        image.setAttribute("src", imagesrc);
+        image.setAttribute("alt", desc);
+        table.appendChild(image);
+        // document.querySelector("tr.temperature").appendChild(table);
+
+        let p = document.createElement("p");
+        p.textContent = ((list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(0) + "ºF";
+        // ((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2)
+        table.appendChild(p);
+        
+        //table2.appendChild(table);
+        
+        document.querySelector('div.table').appendChild(table);
+      }
+    }
 
 
 
-
-
-
-
-
-    const imagesrc = 'https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png';  // note the concatenation
-    const desc = jsObject.weather[0].description;  // note how we reference the weather array
-    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-    document.getElementById('icon').setAttribute('alt', desc);
 
 
   });
@@ -57,13 +72,13 @@ fetch(apiURL)
 //   .then(function (jsonObject) {
 //     console.table(jsonObject);  
 //     // temporary checking for valid response and data parsing
-    
+
 //     const towns = jsonObject['towns'];
-   
+
 
 //     for (let i = 0; i < towns.length; i++) {
 //       if(towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven"){
-        
+
 //         let towndata = document.createElement('section');
 //         let h2 = document.createElement('h2');
 //         let h3 = document.createElement('h3');
@@ -71,7 +86,7 @@ fetch(apiURL)
 //         let p2 = document.createElement('p');
 //         let p3 = document.createElement('p');
 //         let image = document.createElement('img');
-        
+
 //         h2.textContent = towns[i].name;
 //         h3.textContent = towns[i].motto;
 //         p1.textContent = 'Year Founded:' + ' ' + towns[i].yearFounded;
@@ -83,22 +98,21 @@ fetch(apiURL)
 //         image.setAttribute('alt', towns[i].name);
 //         // <img src="Prestion.jpg" alt="preston"></img>
 
-        
+
 //         towndata.appendChild(h2);
 //         towndata.appendChild(h3);
 //         towndata.appendChild(p1);
 //         towndata.appendChild(p2);
 //         towndata.appendChild(p3);
 //         towndata.appendChild(image);
-        
+
 //         document.querySelector('div.towndata').appendChild(towndata);
 //       }
-     
-     
-       
+
+
+
 //       } 
-      
 
-  
+
+
 // });
-
--- lesson-11/js/year.js
+++ lesson-11/js/year.js
@@ -1,2 +1,40 @@
 const options = {weekday: 'long', month: 'long', day: 'numeric', year:'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options)
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+
+
+
+let day= new Date();
+
+let dayNames=[
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"
+];
+  
+var nextDay= new Date();
+nextDay.setDate(day.getDate()+1);
+var plusOne=dayNames[nextDay.getDay()];
+document.getElementById("dayPlusOne").innerHTML=plusOne;
+    
+nextDay.setDate(day.getDate()+2);
+var plusTwo=dayNames[nextDay.getDay()];
+document.getElementById("dayPlusTwo").innerHTML=plusTwo;
+
+nextDay.setDate(day.getDate()+3);
+var plusThree=dayNames[nextDay.getDay()];
+document.getElementById("dayPlusThree").innerHTML=plusThree;
+
+nextDay.setDate(day.getDate()+4);
+var plusFour=dayNames[nextDay.getDay()];
+document.getElementById("dayPlusFour").innerHTML=plusFour;
+
+nextDay.setDate(day.getDate()+5);
+var plusFive=dayNames[nextDay.getDay()];
+document.getElementById("dayPlusFive").innerHTML=plusFive;
+
+  
