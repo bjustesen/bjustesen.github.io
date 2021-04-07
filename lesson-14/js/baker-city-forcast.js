84414eec22ef8fdb2f0171e4eb6b2cf175d07628
bjustesen <bog15004@byui.edu>
2021-04-07 15:23:24 -0600

    outline2

---
+++ lesson-14/js/baker-city-forcast.js
@@ -0,0 +1,54 @@
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5712169&appid=979e25236567012c3b8dd149b86738a0';
+const apiForcast = 'https://api.openweathermap.org/data/2.5/forecast?id=5712169&appid=979e25236567012c3b8dd149b86738a0';
+
+
+fetch(apiForcast)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+
+    const list = jsObject['list'];
+
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].dt_txt.includes('18:00:00')) {
+
+        let table = document.createElement("section");
+
+
+        const imagesrc = "https://openweathermap.org/img/w/" + jsObject.list[i].weather[0].icon + ".png"; //note the concatenation
+        const desc = jsObject.list[i].weather[0].description;
+        let image = document.createElement("img");
+        image.setAttribute("src", imagesrc);
+        image.setAttribute("alt", desc);
+        table.appendChild(image);
+        // document.querySelector("tr.temperature").appendChild(table);
+
+        let p = document.createElement("p");
+        p.textContent = ((list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(0) + "ºF";
+        // ((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2)
+        table.appendChild(p);
+        
+        //table2.appendChild(table);
+        
+        document.querySelector('div.table').appendChild(table);
+      }
+    }
+  });
+
+
+  fetch(apiURL)
+.then((response)=> response.json())
+.then((jsObject)=>{
+    console.log(jsObject)
+    document.getElementById("weather").textContent=jsObject.weather[0].description;
+
+    document.getElementById("current").textContent=((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2)+ "ºF";
+    
+
+    document.getElementById("high").textContent=((jsObject.main.temp_max-273.15)*(9/5)+32).toFixed(2);
+    document.getElementById("humidity").textContent=jsObject.main.humidity+"%";
+    document.getElementById("windspeed").textContent=jsObject.wind.speed;
+    document.getElementById("windchill").textContent=doInputOutput(((jsObject.main.temp-273.15)*(9/5)+32),jsObject.wind.speed);
+});
+
+
