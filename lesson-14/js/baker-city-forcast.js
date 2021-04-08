84408abf578389795f259c8a98a6060e7900dc32
bjustesen <bog15004@byui.edu>
2021-04-07 21:48:57 -0600

    things

--- lesson-14/js/baker-city-forcast.js
+++ lesson-14/js/baker-city-forcast.js
@@ -3,52 +3,46 @@ const apiForcast = 'https://api.openweathermap.org/data/2.5/forecast?id=5712169&
 
 
 fetch(apiForcast)
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-
-    const list = jsObject['list'];
-
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const list = jsonObject["list"];
     for (let i = 0; i < list.length; i++) {
-      if (list[i].dt_txt.includes('18:00:00')) {
+      let extract = list[i].dt_txt.substr(11);
+      if (extract == "18:00:00") {
+        let card = document.createElement("th");
+        let td = document.createElement("td");
+        td.textContent = ((list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(0) + "ºF";
+        card.appendChild(td);
 
-        let table = document.createElement("section");
-
-
-        const imagesrc = "https://openweathermap.org/img/w/" + jsObject.list[i].weather[0].icon + ".png"; //note the concatenation
-        const desc = jsObject.list[i].weather[0].description;
+        const imagesrc = "https://openweathermap.org/img/w/" + jsonObject.list[i].weather[0].icon + ".png"; //note the concatenation
+        const desc = jsonObject.list[i].weather[0].description;
         let image = document.createElement("img");
         image.setAttribute("src", imagesrc);
         image.setAttribute("alt", desc);
-        table.appendChild(image);
-        // document.querySelector("tr.temperature").appendChild(table);
+        card.appendChild(image);
 
-        let p = document.createElement("p");
-        p.textContent = ((list[i].main.temp - 273.15) * (9 / 5) + 32).toFixed(0) + "ºF";
-        // ((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2)
-        table.appendChild(p);
-        
-        //table2.appendChild(table);
-        
-        document.querySelector('div.table').appendChild(table);
+       
+        document.querySelector("table").appendChild(card);
+        document.querySelector("table").appendChild(card);
       }
     }
   });
 
 
-  fetch(apiURL)
-.then((response)=> response.json())
-.then((jsObject)=>{
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
     console.log(jsObject)
-    document.getElementById("weather").textContent=jsObject.weather[0].description;
+    document.getElementById("weather").textContent = jsObject.weather[0].description;
 
-    document.getElementById("current").textContent=((jsObject.main.temp-273.15)*(9/5)+32).toFixed(2)+ "ºF";
-    
-
-    document.getElementById("high").textContent=((jsObject.main.temp_max-273.15)*(9/5)+32).toFixed(2);
-    document.getElementById("humidity").textContent=jsObject.main.humidity+"%";
-    document.getElementById("windspeed").textContent=jsObject.wind.speed;
-    document.getElementById("windchill").textContent=doInputOutput(((jsObject.main.temp-273.15)*(9/5)+32),jsObject.wind.speed);
-});
+    document.getElementById("current").textContent = ((jsObject.main.temp - 273.15) * (9 / 5) + 32).toFixed(2) + "ºF";
 
 
+    document.getElementById("high").textContent = ((jsObject.main.temp_max - 273.15) * (9 / 5) + 32).toFixed(2);
+    document.getElementById("humidity").textContent = jsObject.main.humidity + "%";
+    document.getElementById("windspeed").textContent = jsObject.wind.speed;
+    document.getElementById("windchill").textContent = doInputOutput(((jsObject.main.temp - 273.15) * (9 / 5) + 32), jsObject.wind.speed);
+  });
